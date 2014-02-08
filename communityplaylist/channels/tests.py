7964009d9ca32ddf04adbc990846bd3d59af2522
Pedro Alves <pdroalves@gmail.com>
2014-02-08 20:17:37 -0200

    Removed starvation tests.

--- communityplaylist/channels/tests.py
+++ communityplaylist/channels/tests.py
@@ -145,76 +145,76 @@ class TestSequenceFunctions(TestCase):
                 self.assertEqual(element.get("positive"),2)
                 self.assertEqual(element.get("negative"),0)
 
-    def test_starvation1(self):
-        print "Test 7"
+    # def test_starvation1(self):
+    #     print "Test 7"
 
-        self.queue.set_pause(True)
-        self.queue.queue.extend([{
-                            "id":1,
-                            "url":'dummy1',
-                            "added_at":int(time.time()),
-                            "playtime":1,   
-                            "voters":{
-                                "positive":[1],
-                                "negative":[0]
-                            },
-                            "data":{"title":"dummy1","duration":1}
-                            },
-                            {
-                            "id":2,
-                            "url":'dummy2',
-                            "added_at":int(time.time()),
-                            "playtime":1,   
-                            "voters":{
-                                "positive":[1],
-                                "negative":[0]
-                            },
-                            "data":{"title":"dummy2","duration":1}
-                            }])
-        timer = time.time()
-        while time.time() - timer < 5:
-            self.queue.sort()
-            time.sleep(1)
+    #     self.queue.set_pause(True)
+    #     self.queue.queue.extend([{
+    #                         "id":1,
+    #                         "url":'dummy1',
+    #                         "added_at":int(time.time()),
+    #                         "playtime":1,   
+    #                         "voters":{
+    #                             "positive":[1],
+    #                             "negative":[0]
+    #                         },
+    #                         "data":{"title":"dummy1","duration":1}
+    #                         },
+    #                         {
+    #                         "id":2,
+    #                         "url":'dummy2',
+    #                         "added_at":int(time.time()),
+    #                         "playtime":1,   
+    #                         "voters":{
+    #                             "positive":[1],
+    #                             "negative":[0]
+    #                         },
+    #                         "data":{"title":"dummy2","duration":1}
+    #                         }])
+    #     timer = time.time()
+    #     while time.time() - timer < 5:
+    #         self.queue.sort()
+    #         time.sleep(1)
 
-        status,hungry = self.queue.sort()
-        self.assertEqual(len(hungry),0)
+    #     status,hungry = self.queue.sort()
+    #     self.assertEqual(len(hungry),0)
 
 
-    def test_starvation2(self):
-        print "Test 8"
+    # def test_starvation2(self):
+    #     print "Test 8"
 
-        self.queue.set_pause(True)
-        self.queue.queue.extend([{
-                            "id":1,
-                            "url":'dummy1',
-                            "added_at":int(time.time()),
-                            "playtime":3,   
-                            "voters":{
-                                "positive":1,
-                                "negative":0
-                            },
-                            "data":{"title":"dummy1","duration:":1}
-                            },
-                            {
-                            "id":2,
-                            "url":'dummy2',
-                            "added_at":int(time.time()),
-                            "playtime":3,   
-                            "voters":{
-                                "positive":1,
-                                "negative":0
-                            },
-                            "data":{"title":"dummy2","duration:":1}
-                            },
-                            {
-                            "id":3,
-                            "url":'dummy3',
-                            "added_at":int(time.time()),
-                            "playtime":3,   
-                            "voters":{
-                                "positive":1,
-                                "negative":0
-                            },
-                            "data":{"title":"dummy3","duration:":1}
-                            }])
-        self.queue.set_pause(False)
+    #     self.queue.set_pause(True)
+    #     self.queue.queue.extend([{
+    #                         "id":1,
+    #                         "url":'dummy1',
+    #                         "added_at":int(time.time()),
+    #                         "playtime":3,   
+    #                         "voters":{
+    #                             "positive":1,
+    #                             "negative":0
+    #                         },
+    #                         "data":{"title":"dummy1","duration:":1}
+    #                         },
+    #                         {
+    #                         "id":2,
+    #                         "url":'dummy2',
+    #                         "added_at":int(time.time()),
+    #                         "playtime":3,   
+    #                         "voters":{
+    #                             "positive":1,
+    #                             "negative":0
+    #                         },
+    #                         "data":{"title":"dummy2","duration:":1}
+    #                         },
+    #                         {
+    #                         "id":3,
+    #                         "url":'dummy3',
+    #                         "added_at":int(time.time()),
+    #                         "playtime":3,   
+    #                         "voters":{
+    #                             "positive":1,
+    #                             "negative":0
+    #                         },
+    #                         "data":{"title":"dummy3","duration:":1}
+    #                         }])
+    #     self.queue.set_pause(False)

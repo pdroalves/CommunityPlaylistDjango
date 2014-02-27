ab57740fbd9e4c52e7a31b79ac49e830e95f7392
Pedro Alves <pdroalves@gmail.com>
2014-02-27 16:12:32 -0300

    - Now we have a (almost plain) home page. - Added some CSS for the home page.

--- communityplaylist/main/views.py
+++ communityplaylist/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import os
+import logging
 
 # Create your views here.
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+logger = logging.getLogger("MainView")
+
+def index(request):
+	context = {"title":"CommunityPlaylist"}
+	return render(request,'main/index.html',context)

18a6b31fd35d734fcf45a17e6041d93ac59bf629
Pedro Alves <pdroalves@gmail.com>
2014-02-27 13:49:11 -0300

    Some static files were moved to a common directory.\nAdded main app to the project.

--- communityplaylist/communityplaylist/settings.py
+++ communityplaylist/communityplaylist/settings.py
@@ -87,6 +87,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+	BASE_DIR+'/common-static',	
+)
 
 # Logging
 #
--- communityplaylist/communityplaylist/urls.py
+++ communityplaylist/communityplaylist/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^channels/',include('channels.urls',namespace="channels"))
+    url(r'^$',include('main.urls',namespace="main")),
+    url(r'^channels/',include('channels.urls',namespace="channels")),
 )
